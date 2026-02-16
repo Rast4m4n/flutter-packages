9c94575d33e959a4e3ab71495f1f3d81522de348
Motovilin Kirill <motovilinkirill@gitlab.local>
2026-02-16 10:49:32 +0300

    added table indention in horizontal

--- packages/docx_creator/lib/src/ast/docx_table.dart
+++ packages/docx_creator/lib/src/ast/docx_table.dart
@@ -144,6 +144,9 @@ class DocxTablePosition {
   /// Y position in twips (from the vertical anchor).
   final int? tblpY;
 
+  /// X table indention in twips.
+  final int? tblInd;
+
   /// Left margin from surrounding text in twips.
   final int leftFromText;
 
@@ -165,6 +168,7 @@ class DocxTablePosition {
     this.rightFromText = 180,
     this.topFromText = 0,
     this.bottomFromText = 0,
+    this.tblInd,
   });
 
   /// Center the table horizontally.
@@ -392,6 +396,9 @@ class DocxTable extends DocxBlock {
                       'w:bottomFromText', position!.bottomFromText.toString());
                   builder.attribute('w:vertAnchor', position!.vAnchor.name);
                   builder.attribute('w:horzAnchor', position!.hAnchor.name);
+                  if (position!.tblInd != null) {
+                    builder.attribute('w:tblInd', position!.tblInd.toString());
+                  }
                   if (position!.tblpX != null) {
                     builder.attribute('w:tblpX', position!.tblpX.toString());
                   }
